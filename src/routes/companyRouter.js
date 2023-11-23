97da806a79736d1dfd679bbfaf64e5efec1baeda
nndang1229 <nndang.1229@indivisys.jp>
2023-11-23 15:52:53 +0700

    update

--- src/routes/companyRouter.js
+++ src/routes/companyRouter.js
@@ -2,17 +2,35 @@ import express from "express";
 import { isAuthenticated, isAdmin } from "../Middlewares/auth.js";
 import companyController from "../Controllers/companyController.js";
 
-const { createCampany, showCompanies } = companyController;
+const {
+  createCampany,
+  showCompanies,
+  singleCompany,
+  updateCompany,
+  deleteCompany,
+} = companyController;
 const router = express.Router();
-//jobs routes
+//company routes
 
 // /api/company/create
 router.post("/company/create", isAuthenticated, isAdmin, createCampany);
-// // /api/job/id
-// router.get("/job/:id", singleJob);
-// // /api/job/update/job_id
-// router.put("/job/update/:job_id", isAuthenticated, isAdmin, updateJob);
-// // /api/jobs/show
-router.get("/companies/show", showCompanies);
+// /api/company/id
+router.get("/company/:id", singleCompany);
+// /api/company/update/company_id
+router.put(
+  "/company/update/:company_id",
+  isAuthenticated,
+  isAdmin,
+  updateCompany
+);
+// /api/companies
+router.get("/companies", showCompanies);
+// /api/company/delete/company_id
+router.delete(
+  "/company/delete/:company_id",
+  isAuthenticated,
+  isAdmin,
+  deleteCompany
+);
 
 export default router;
