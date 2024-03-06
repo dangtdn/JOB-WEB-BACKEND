3e541ed50673fe01f74704b9c7c494dc72958662
nndang1229 <nndang.1229@indivisys.jp>
2024-03-06 17:27:40 +0700

    update API resumes

--- src/routes/resumeRouter.js
+++ src/routes/resumeRouter.js
@@ -1,21 +1,29 @@
 import express from "express";
 import ResumeController from "../Controllers/resumeController.js";
 
-const { getResumePrivate, createResume, updateResume, deleteResume } =
-  ResumeController;
+const {
+  getResumePrivate,
+  createResume,
+  updateResume,
+  deleteResume,
+  getSingleResume,
+} = ResumeController;
 const router = express.Router();
 //resumes routes
 
 // /api/resumes
 router.get("/resumes", getResumePrivate);
 
+// /api/resumes/:id
+router.get("/resumes/:id", getSingleResume);
+
 // /api/resumes/create
 router.post("/resumes/create", createResume);
 
 // /api/resumes/:id/update
-router.post("/resumes/:id/update", updateResume);
+router.put("/resumes/:id/update", updateResume);
 
 // /api/resumes/:id/delete
-router.post("/resumes/:id/delete", deleteResume);
+router.delete("/resumes/:id/delete", deleteResume);
 
 export default router;
