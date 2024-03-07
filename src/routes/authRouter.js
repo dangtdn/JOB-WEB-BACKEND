18b2e0da09838de4ddcccd86ea0e5fd880453f50
Nguyen Nhat Dang(IVS) <nndang.0227@vitenet.net>
2024-03-07 23:35:37 +0700

    update

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import authController from "../Controllers/authController.js";
 
@@ -16,23 +15,3 @@ router.get("/logout", logout);
 router.get("/user-profile", userProfile);
 
 export default router;
-=======
-import express from "express";
-import authController from "../Controllers/authController.js";
-import { isAuthenticated } from "../Middlewares/auth.js";
-
-const { signup, signin, logout, userProfile } = authController;
-const router = express.Router();
-
-//auth routes
-// /api/signup
-router.post("/register", signup);
-// /api/signin
-router.post("/login", signin);
-// /api/logout
-router.get("/logout", logout);
-// // /api/me
-router.get("/userprofile", isAuthenticated, userProfile);
-
-export default router;
->>>>>>> 665abd5e0b0aabe0c044757701071e51f30ec5df
